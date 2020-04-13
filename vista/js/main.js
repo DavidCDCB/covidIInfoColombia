70258f88858038a0426673a05ad88c716414dae0
DavidCDCB <cristian-cb1@hotmail.com>
2020-04-13 12:08:35 -0500

    Server

--- vista/js/main.js
+++ vista/js/main.js
@@ -11,14 +11,23 @@
 import { Grafo } from "./Grafo.js";
 import dom from './dom-pruebas.js';//clase estatica
 
-((doc, win) => {
-	doc.addEventListener('DOMContentLoaded',event => {
 
-		dom.getElemento("totales").innerText="1223";
-		peticion('https://corona.lmao.ninja/countries/colombia').then(result =>{
-			
-			console.log(result.cases);
+
+((doc, win) => {
 	
+	doc.addEventListener('DOMContentLoaded',event => {
+		
+		peticion('https://corona.lmao.ninja/countries/colombia').then(result =>{
+			fetch('https://cors-anywhere.herokuapp.com/' + 'http://unremoved-sediments.000webhostapp.com/BD.json').then(blob => blob.json()).then(ext => {
+				if(ext.nombre==result.cases){
+					dom.getElemento("totales").innerText=result.cases;
+					dom.getElemento("recuperados").innerText=result.recovered;
+					dom.getElemento("muertes").innerText=result.deaths;
+				}else{
+
+					almacenar("https://unremoved-sediments.000webhostapp.com/server.php?nombre="+result.cases+"&almacenarDatos=si");
+				}
+			});
 		});
 	});
 
@@ -27,6 +36,29 @@ import dom from './dom-pruebas.js';//clase estatica
 			return response.json();
 		});
 		return response;
+	}
+
+	const almacenar = async (url) => {
+		var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+		fetch(proxyUrl + url).then(data => {
+			console.log(data);
+			return data;
+		}).catch(e => {
+		});
+	}
+
+	const cargar = async (url) => {
+		var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+		const response = await fetch(proxyUrl + url).then(blob => blob.json()).then(data => {
+			//console.table(data);
+			casosT=data.nombre;
+			return data;
+		})
+		.catch(e => {
+			console.log(e);
+			return e;
+		});
+		console.log(casosT);
 	}
 })(document, window);
 
